d449de2f39542fd806814776d55e5e28f467d606
iki-taichi <taichi.iki@gmail.com>
2021-09-18 17:51:40 +0900

    fix the script path in batch_run.sh for GLUE

--- evaluation/glue_tasks/batch_run.sh
+++ evaluation/glue_tasks/batch_run.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 # Batch run script for the GLUE Benchmark
+# This script iterates training num_prefixes x num_tasks x num_models times
+#
 # To set visible GPUs, add CUDA_VISIBLE_DEVICES=X when calling this command:
 # CUDA_VISIBLE_DEVICES=0,1 evalutation/batch_run.sh
-# This script iterates training num_prefixes x num_tasks x num_models times
 
 # Settings
+SCRIPT_PATH="evaluation/glue_tasks/run_glue.py"
+
 # - Tasks to be used
 tasks="stsb mrpc cola wnli sst2 qnli rte qqp mnli"
 
 # - Models to be trained
-pt_models="ctrl_lxmert_base ctrl_uniter_base ctrl_vilbert_base ctrl_visual_bert_base ctrl_vl_bert_base"
-reinit_models="ctrl_lxmert_base_reinit ctrl_uniter_base_reinit ctrl_vilbert_base_reinit ctrl_visual_bert_base_reinit ctrl_vl_bert_base_reinit"
+pt_models="ctrl_lxmert ctrl_uniter ctrl_vilbert ctrl_visual_bert ctrl_vl_bert"
+reinit_models="ctrl_lxmert_reinit ctrl_uniter_reinit ctrl_vilbert_reinit ctrl_visual_bert_reinit ctrl_vl_bert_reinit"
 models="$pt_models $reinit_models"
 
 # - Prefix for the run (should be integers)
 prefixes="0 1 2"
 
 # Debug settings
-tasks="cola"
-models="ctrl_lxmert"
-prefixes="0"
+#tasks="cola"
+#models="ctrl_vl_bert"
+#prefixes="0"
 
 # Directories
 pretrained_models_dir="vl_models/pretrained"
@@ -35,7 +38,7 @@ for prefix in $prefixes ; do
             model_path="$pretrained_models_dir/$model"
             
             echo "$prefix $task_name $model > $output_dir"
-            python -u evaluation/run_glue.py \
+            python -u "$SCRIPT_PATH" \
                 --model_name_or_path "$model_path" \
                 --task_name "$task_name" \
                 --do_train \
